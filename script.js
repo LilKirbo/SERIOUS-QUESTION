0bcd98ae25234f4c220fb99baa705eedb4c47399
LilKirbo <lok.t1mmy@hotmail.com>
2026-02-08 13:13:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 // Function to handle button click events
 function selectOption(option) {
     // Check which option was clicked
-    if (option === 'yes') {
+    if (option === 'YUSSS') {
         // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
             displayCatHeart(); // Display the cat-heart.gif
         });
-    } else if (option === 'no') {
+    } else if (option === 'NUUU') {
         // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        document.getElementById('no-button').innerText = 'WHATTTTTT?'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
@@ -77,4 +77,5 @@ function displayCatHeart() {
 }
 
 // Display the cat.gif initially
+
 displayCat();
